22b698d9965e992be6fd5e161cf297d1e11f4833
artikor <art-55@mail.ru>
2018-07-08 19:34:56 +0300

    Revert "start diplom project"
    This reverts commit 50c100b09855c26c257be4cbb7222d7809add861.

--- techno-ostrov.by/resources/views/Web/Pages/catalog.blade.php
+++
@@ -1,8 +0,0 @@
-@extends("Web/layouts/template")
-
-@section("description","About")
-@section("title","Main page")
-
-@section("content")
-<h1>Каталог</h1>	
-@endsection
--- techno-ostrov.by/resources/views/Web/Pages/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("Web/layouts/template")
-
-@section("description","About")
-@section("title","Main page")
-
-@section("content")
-<h1>Главная страница</h1>
-	
-@endsection
